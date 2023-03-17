98e6681b0416e509ff5f6d2150bc13d1e1a8b78c
JangKwangHee <devjang2021@gmail.com>
2023-03-17 14:50:21 +0900

    독립 실행형 스프링 애플리케이션 강의 적용

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # 토비의 Springboot 강의
 
 ### 연습 코드
+
+#### 독립 실행형 스프링 애플리케이션
+
+- HelloController 를 컨테이너에 직접 bean 으로 등록
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,10 +1,8 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.http.HttpHeaders;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -17,19 +15,25 @@ import java.io.IOException;
 public class HellobootApplication {
 
 	public static void main(String[] args) {
+		// 스프링 컨테이너 만들기
+		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		applicationContext.registerBean(HelloController.class);		// class 정보만 넘김
+		applicationContext.refresh();	// 구성정보를 이용해 bean object 만들어 초기화
+
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
 			servletContext.addServlet("hello", new HttpServlet() {
-				HelloController helloController = new HelloController();
 
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 					// 인증, 보안, 다국어, 공통 기능
 
 					if (req.getRequestURI().equals("/hello") ) {
+						// binding - 새로운 타입으로 변환하여 넘겨주는 작업
 						String name = req.getParameter("name");
 
-						// binding - 새로운 타입으로 변환하여 넘겨주는 작업
+						// 스프링 컨테이너를 사용해 ojbect 를 받아옴
+						HelloController helloController = applicationContext.getBean(HelloController.class);
 						String ret = helloController.hello(name);
 
 //						resp.setStatus(HttpStatus.OK.value());
