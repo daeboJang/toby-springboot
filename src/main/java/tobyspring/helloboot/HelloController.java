b7259a5742d36b8bfb4905bc16c3b47dbf6f2d8a
JangKwangHee <devjang2021@gmail.com>
2023-03-17 15:44:17 +0900

    의존 오브젝트 추가 강의까지
    - SimpleHelloService 클래스 추가후 컨트롤러에서 사용하도록 변경

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,7 +1,12 @@
 package tobyspring.helloboot;
 
+import java.util.Objects;
+
 public class HelloController {
     public String hello(String name) {
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        // null일 경우 예외발생
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
